2d863c7f26e46d6ca5bc4067d892312e3b0c489a
ro-hit2023 <kumaranand0089@gmail.com>
2023-11-28 13:04:23 +0530

    Repositories for models

--- src/main/java/com/scaler/bookmyshow/repositroy/ShowRepository.java
+++ src/main/java/com/scaler/bookmyshow/repositroy/ShowRepository.java
@@ -1,10 +1,11 @@
 package com.scaler.bookmyshow.repositroy;
 
 import com.scaler.bookmyshow.models.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ShowRepository extends JpaRepository<Show, Long{
+public interface ShowRepository extends JpaRepository<Show, Long> {
     @Override
     Optional<Show> findById(Long showId);
 
